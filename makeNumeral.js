b258dfe6c94e603e3090644cf7103b3c179e7b49
S-Spiegl <steven.spiegl@gmail.com>
2022-09-18 00:04:51 +0100

    using for loop for 1-3, hardcoding 4-5

--- makeNumeral.js
+++ makeNumeral.js
@@ -15,9 +15,15 @@ makeNumeral = (input) => {
   if(input <= 3){
     for(let i = 0; i < input; i++){
       output.push('I')
+    }
+  }
+  if(input == 4){
+    output.push('IV')
+  }
+  if(input == 5){
+    output.push('V')
   }
   return output.join('')
-  }
 }
 
 module.exports = makeNumeral
